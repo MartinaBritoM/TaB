ff50fd03f90c9110f8ff593dd57249bb36456cbf
Sofia-Bastias <75391432+Sofia-Bastias@users.noreply.github.com>
2020-12-12 21:42:05 -0300

    Update tab5.page.ts

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { ELocalNotificationTriggerUnit, LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { CallNumber } from '@ionic-native/call-number/ngx';
 
 @Component({
-  selector: 'app-tab5',
-  templateUrl: 'tab5.page.html',
-  styleUrls: ['tab5.page.scss']
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
 })
-export class Tab5Page {
+export class Tab1Page {
+  lat;
+  lng;
 
-  constructor() {}
+  constructor(
+    private geo: Geolocation,private localNotifications: LocalNotifications,private callNumber: CallNumber
+  ) { }
+
+  registerNotification(seconds:number) {
+    this.localNotifications.schedule({
+      title: "Listo!",
+      text:"Ubicación enviada",
+      trigger: {
+        in: seconds,
+        unit: ELocalNotificationTriggerUnit.SECOND,
+      },
+    });
+  }
+
+  callContact(number: string) {
+    this.callNumber.callNumber("975308275", true)
+      .then(() => console.log('Dialer Launched!'))
+      .catch(() => console.log('Error launching dialer'));
+  }
+
+  whereiam()
+  {
+    this.geo.getCurrentPosition({
+      timeout: 10000,
+      enableHighAccuracy: true
+    }).then((res)=>{
+      this.lat =res.coords.latitude;
+      this.lng =res.coords.longitude;
+    }).catch((e)=>{
+      console.log(e);
+    });
+  }
 
 }
